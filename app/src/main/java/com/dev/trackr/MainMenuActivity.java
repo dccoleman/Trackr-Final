e7d6733803f286b77d6e76e3bf8ac4669c9576e8
dccoleman <dccoleman@wpi.edu>
2017-02-27 15:44:30 -0500

    Updated Main Activity
    TODO:
    Add intent -> map activity

---
+++ app/src/main/java/com/dev/trackr/MainMenuActivity.java
@@ -0,0 +1,35 @@
+package com.dev.trackr;
+
+import android.location.Location;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.orm.SugarRecord;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu_activity);
+
+        Button createButton = (Button) findViewById(R.id.createAdventure);
+        createButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+
+
+        }
+
+}
